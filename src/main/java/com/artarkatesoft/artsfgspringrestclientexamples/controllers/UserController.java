e5d1ee387cb8e7ef840eded651c82ab975e591a5
Art <d.art.shishkin@gmail.com>
2020-08-10 18:04:01 +0300

    added UserControllerSpringBootTest (368). #5

--- src/main/java/com/artarkatesoft/artsfgspringrestclientexamples/controllers/UserController.java
+++ src/main/java/com/artarkatesoft/artsfgspringrestclientexamples/controllers/UserController.java
@@ -1,16 +1,16 @@
 package com.artarkatesoft.artsfgspringrestclientexamples.controllers;
 
-import com.artarkatesoft.api.domain.User;
 import com.artarkatesoft.artsfgspringrestclientexamples.ApiService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
-import java.util.List;
-
+@Slf4j
 @Controller
 @RequestMapping("users")
 @RequiredArgsConstructor
@@ -19,11 +19,13 @@ public class UserController {
     private final ApiService apiService;
 
     @PostMapping
-    public String getAllUsers(Model model, ServerWebExchange serverWebExchange) {
-        Integer limit = serverWebExchange.getAttribute("limit");
-        if (limit == null || limit < 1) limit = 1;
-        List<User> requestedUsers = apiService.getUsers(limit);
-        model.addAttribute("users", requestedUsers);
-        return "users";
+    public Mono<String> getAllUsers(Model model, ServerWebExchange serverWebExchange) {
+        Mono<Integer> limitMono = serverWebExchange.getFormData()
+                .map(multiValueMap -> multiValueMap.getFirst("limit"))
+                .log()
+                .map(Integer::valueOf);
+        return limitMono.map(apiService::getUsers)
+                .doOnNext(users -> model.addAttribute("users", users))
+                .then(Mono.just("users"));
     }
 }
---
+++ src/test/java/com/artarkatesoft/artsfgspringrestclientexamples/controllers/UserControllerSpringBootTest.java
@@ -0,0 +1,50 @@
+package com.artarkatesoft.artsfgspringrestclientexamples.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class UserControllerSpringBootTest {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    private WebTestClient webTestClient;
+
+    @BeforeEach
+    void setUp() {
+        webTestClient = WebTestClient.bindToApplicationContext(applicationContext).build();
+    }
+
+    @Test
+    void index() {
+        //when
+        webTestClient.get().uri("/")
+                .exchange()
+                //then
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .value(content -> assertThat(content).contains("<h1>Home Page</h1>"));
+    }
+
+    @Test
+    void testPost() {
+        //given
+        Integer limit = 2;
+        //when
+        webTestClient.post().uri("/users")
+                .body(BodyInserters.fromFormData("limit", limit.toString()))
+                .exchange()
+                //then
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .value(content -> assertThat(content).contains("<h1>User Information</h1>"));
+    }
+}
