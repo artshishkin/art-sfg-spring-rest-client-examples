f92721404c0e34f4d6207584a2c5d152934b897c
Art <d.art.shishkin@gmail.com>
2020-08-10 23:46:39 +0300

    RestTemplate  Examples (PUT) (370). #7

--- src/test/java/com/artarkatesoft/artsfgspringrestclientexamples/examples/RestTemplateExamplesTest.java
+++ src/test/java/com/artarkatesoft/artsfgspringrestclientexamples/examples/RestTemplateExamplesTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -84,9 +86,72 @@ class RestTemplateExamplesTest {
                 () -> assertThat(savedCustomerNode.get("customer_url").asText()).startsWith("/shop/customers/")
         );
         //then find customer by id
-        url = fromHttpUrl(BASE_URL).replacePath("/shop/customers/382").toUriString();
+        String customer_url = savedCustomerNode.get("customer_url").asText();
+        url = fromHttpUrl(BASE_URL).replacePath(customer_url).toUriString();
         String retrievedCustomer = restTemplate.getForObject(url, String.class);
         System.out.println(retrievedCustomer);
         assertThat(retrievedCustomer).isNotEmpty().contains("Art", "Shyshkin", "lastname", "firstname");
+    }
+
+    @Test
+    void updateCustomer_usingPut() {
+        //given
+        String customerJson = "{\"firstname\": \"Art\",\"lastname\": \"Shyshkin\"}";
+        String url = fromHttpUrl(BASE_URL).path("/customers/").toUriString();
+        ResponseEntity<JsonNode> responseEntity = restTemplate.postForEntity(url, customerJson, JsonNode.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(CREATED);
+        JsonNode savedCustomerNode = responseEntity.getBody();
+        assertThat(savedCustomerNode).isNotNull();
+        assertAll(
+                () -> assertThat(savedCustomerNode.get("firstname").asText()).isEqualTo("Art"),
+                () -> assertThat(savedCustomerNode.get("lastname").asText()).isEqualTo("Shyshkin"),
+                () -> assertThat(savedCustomerNode.get("customer_url").asText()).startsWith("/shop/customers/")
+        );
+        //when update customer by id
+        String customerUrl = savedCustomerNode.get("customer_url").asText();
+
+        url = fromHttpUrl(BASE_URL).replacePath(customerUrl).toUriString();
+
+        String customerModifiedJson = "{\"firstname\": \"Art 2\",\"lastname\": \"Shyshkin 2\"}";
+        restTemplate.put(url, customerModifiedJson);
+
+        //then find customer by id
+        String retrievedCustomer = restTemplate.getForObject(url, String.class);
+        System.out.println(retrievedCustomer);
+        assertThat(retrievedCustomer).isNotEmpty().contains("Art 2", "Shyshkin 2", "lastname", "firstname");
+    }
+
+    @Test
+    void updateCustomer_usingExchangePut() {
+        //given
+        String customerJson = "{\"firstname\": \"Art\",\"lastname\": \"Shyshkin\"}";
+        String url = fromHttpUrl(BASE_URL).path("/customers/").toUriString();
+        ResponseEntity<JsonNode> responseEntity = restTemplate.postForEntity(url, customerJson, JsonNode.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(CREATED);
+        JsonNode savedCustomerNode = responseEntity.getBody();
+        assertThat(savedCustomerNode).isNotNull();
+        assertAll(
+                () -> assertThat(savedCustomerNode.get("firstname").asText()).isEqualTo("Art"),
+                () -> assertThat(savedCustomerNode.get("lastname").asText()).isEqualTo("Shyshkin"),
+                () -> assertThat(savedCustomerNode.get("customer_url").asText()).startsWith("/shop/customers/")
+        );
+        //when update customer by id
+        String customerUrl = savedCustomerNode.get("customer_url").asText();
+
+        url = fromHttpUrl(BASE_URL).replacePath(customerUrl).toUriString();
+
+        String customerModifiedJson = "{\"firstname\": \"Art 2\",\"lastname\": \"Shyshkin 2\"}";
+        HttpEntity<String> requestEntity = new HttpEntity<>(customerModifiedJson);
+        ResponseEntity<String> stringResponseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, String.class);
+
+        //then
+        assertThat(stringResponseEntity.getStatusCode()).isEqualTo(OK);
+
+        String updatedCustomerString = stringResponseEntity.getBody();
+        System.out.println(updatedCustomerString);
+        assertThat(updatedCustomerString)
+                .isNotEmpty()
+                .contains("Art 2", "Shyshkin 2", "lastname", "firstname")
+                .doesNotContain("/shop/customers/", "customer_url");
     }
 }
