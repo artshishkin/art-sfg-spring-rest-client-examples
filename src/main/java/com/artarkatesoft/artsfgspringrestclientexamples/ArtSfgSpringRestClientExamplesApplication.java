3d893996d6b6fbae311e0a96c7c28b06a3b24a46
Art <d.art.shishkin@gmail.com>
2020-08-10 11:32:32 +0300

    Created Spring Boot Project (365). #1

---
+++ src/main/java/com/artarkatesoft/artsfgspringrestclientexamples/ArtSfgSpringRestClientExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.artsfgspringrestclientexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSfgSpringRestClientExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtSfgSpringRestClientExamplesApplication.class, args);
+    }
+
+}
