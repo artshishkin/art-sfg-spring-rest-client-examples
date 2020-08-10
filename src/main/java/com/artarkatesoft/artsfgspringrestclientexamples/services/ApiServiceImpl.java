d909e7299588f9e4f8b6114e4ee6d84cbb22906d
Art <d.art.shishkin@gmail.com>
2020-08-10 20:51:09 +0300

    URI Component Builder (369). closes #6

--- src/main/java/com/artarkatesoft/artsfgspringrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/com/artarkatesoft/artsfgspringrestclientexamples/services/ApiServiceImpl.java
@@ -2,22 +2,29 @@ package com.artarkatesoft.artsfgspringrestclientexamples.services;
 
 import com.artarkatesoft.api.domain.User;
 import com.artarkatesoft.api.domain.UserData;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class ApiServiceImpl implements ApiService {
 
     private final RestTemplate restTemplate;
+    private final String apiUrl;
+
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String apiUrl) {
+        this.restTemplate = restTemplate;
+        this.apiUrl = apiUrl;
+    }
 
     @Override
     public List<User> getUsers(Integer limit) {
+        String query = UriComponentsBuilder.fromHttpUrl(apiUrl).queryParam("limit", limit).toUriString();
         UserData userData = restTemplate
-                .getForObject("http://private-211c3e-apifaketory.apiary-mock.com/api/user?limit=" + limit, UserData.class);
+                .getForObject(query, UserData.class);
         return userData.getData();
     }
 }
